ae5c0b96040e34c0f11e78a763a6422f5f9eb5ed
Goldiexe <101436342+Tubers90Mob@users.noreply.github.com>
2022-04-04 17:27:03 -0400

    Update da-rpg-game.py

--- da-rpg-game.py
+++ da-rpg-game.py
@@ -1,10 +1,12 @@
+# Run this game if your on mobile goto a app called "Pydroid 3" if you can't download it
+Goto a apk file by searching "Pydroid 3 apk"
 """
 In this simple RPG game, the hero fights the goblin. He has the options to:
-
+-
 1. fight goblin
 2. do nothing - in which case the goblin will attack him anyway
 3. flee
-
+-
 """
 
 def main():
